98714065a20b5ca4d76f3609b6a48742ad44669b
billowycloud <bjkim0228@naver.com>
2020-06-30 18:02:01 +0900

    change profile style

--- src/Components/Section/Profile.js
+++ src/Components/Section/Profile.js
@@ -12,7 +12,6 @@ const Block = styled.div`
   top: 0;
   color: black;
   padding-top: 3rem;
-
   @media screen and (max-width: 768px) {
     position: unset;
     width: 100%;
@@ -20,34 +19,59 @@ const Block = styled.div`
   }
 `;
 
-const Info = styled.div``;
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 1rem;
+  padding: 10px;
+`;
 
 const Img = styled.div`
   width: 200px;
   height: 200px;
-  border-radius: 100%;
-  background: green;
+  border: 12px solid black;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
 `;
 const Name = styled.div`
-  font-weight: bold;
+  font-weight: 900;
+  font-size: 1.5rem;
 `;
-const Intro = styled.div``;
-const Anchor = styled.div``;
+const Intro = styled.div`
+  margin-top: 2rem;
+  text-align: center;
+  padding: 0 10px;
+`;
+const Anchor = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 const Item = styled.div``;
+const SNS = styled.div`
+  display: flex;
+`;
 
 const Profile = () => {
   return (
     <Block>
       <Info>
+        <Name>웹 개발자 김병준</Name>
         <Img />
-        <Name>김병준 | Byeong Jun, Kim</Name>
-        <Intro></Intro>
+        <Name>Byeong Jun, Kim</Name>
+        <Intro>자기소개입니다. 자기소개입니다. 자기소개입니다. 자기소개입니다.</Intro>
       </Info>
       <Anchor>
         <Item>Skill</Item>
         <Item>Project</Item>
         <Item>Contact</Item>
       </Anchor>
+      <SNS>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </SNS>
     </Block>
   );
 };
